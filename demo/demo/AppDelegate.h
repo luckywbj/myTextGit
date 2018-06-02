37ab54d9966392f2a29af01e74255c335f32ab86
wbj <wangbojun@vipkid.com.cn>
2018-06-02 15:32:32 +0800

    Tijao

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by wbj on 2018/6/2.
+//  Copyright © 2018年 wbj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by wbj on 2018/6/2.
+//  Copyright © 2018年 wbj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
